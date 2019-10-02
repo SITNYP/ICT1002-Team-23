c55b236c2355a9c8f839d97a298583f5da5ea2d3
jellyvongelatin <how2pocketsg@gmail.com>
2019-10-02 15:59:23 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,22 +1,86 @@
 from PyQt4 import QtCore, QtGui, uic
 import pandas as pd
-import matplotlib.pyplot as plt
-import numpy as np
 import modules as mod
 import sys
+import collections
+
+Qt = QtCore.Qt
 
 
-def importCSV(self):
-    options = QFileDialog.Options()
-    fileName, _ = QFileDialog.getOpenFileName(self, "QFileDialog.getOpenFileName()", "", "All Files (*)", options=options)
+class PandasModel(QtCore.QAbstractTableModel):
+    def __init__(self, data, parent=None):
+        QtCore.QAbstractTableModel.__init__(self, parent)
+        self._data = data
+
+    def rowCount(self, parent=None):
+        return len(self._data.values)
+
+    def columnCount(self, parent=None):
+        return self._data.columns.size
+
+    def data(self, index, role=Qt.DisplayRole):
+        if index.isValid():
+            if role == Qt.DisplayRole:
+                return QtCore.QVariant(str(
+                    self._data.values[index.row()][index.column()]))
+        return QtCore.QVariant()
+
+    def sort(self, Ncol, order):
+        try:
+            self.layoutAboutToBeChanged.emit()
+            self._data = self._data.sort_values(self._data.columns[Ncol], ascending=not order)
+            self.layoutChanged.emit()
+        except Exception as e:
+            print(e)
 
 
 class MainWindow(QtGui.QMainWindow):
+    table = pd.DataFrame()
     def __init__(self):
         super(MainWindow, self).__init__()
         uic.loadUi('Main.ui', self)
+        self.setWindowTitle("Team 23 Python Data Analyser")
+
+        self.actionImport_CSV.activated.connect(self.importCSV)  #imports the csv to the program
+        self.actionMerge_CSV.activated.connect(self.mergeCSV)    #merges another csv with the current one
+
         self.show()
 
+    def importCSV(self):  # imports csv and displays as table in GUI
+        try:
+            fileName = mod.openFileLocation(self)
+            self.table = mod.fileUpload(fileName)
+            self.csvTable.setSortingEnabled(True)
+            #self.csvTable.setHorizontalHeaderLabels()
+            self.csvTable.setModel(PandasModel(self.table))
+            self.csvTable.resizeColumnsToContents()
+            self.csvTable.show()
+
+        except Exception as e:
+            #display error gui
+            print e
+
+    def mergeCSV(self):
+        try:
+            fileName = mod.openFileLocation(self)
+            table2 = mod.fileUpload(fileName)
+
+            if collections.Counter(self.table.columns.values) == collections.Counter(table2.columns.values):
+                newTable = pd.concat([self.table, table2])
+                newTable = newTable.reset_index(drop=True)
+            else:
+                newTable = pd.concat([self.table, table2], axis=1)
+
+            self.table = newTable
+
+            self.csvTable.setSortingEnabled(True)
+            self.csvTable.setModel(PandasModel(self.table))
+            self.csvTable.resizeColumnsToContents()
+            self.csvTable.show()
+
+        except Exception as e:
+            #display error gui?
+            print e
 
 if __name__ == '__main__':
     app = QtGui.QApplication(sys.argv)
--- modules.py
+++ modules.py
@@ -1,12 +1,14 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+from PyQt4.QtGui import QFileDialog, QTableWidget, QTableWidgetItem, QSortFilterProxyModel
 
 def fileUpload(filePath): #takes in the file path and returns it as a pandas csv variable
+    #if filePath.lower().endswith('.csv'):
     data = pd.read_csv(filePath)
     return data
 
-
-def fileOutput(dataFrame): #takes in the dataframe and writes to a csv file
-    dataFrame.to_csv('lulw.csv', index=None, header=True)
-    #return "Successfully exported %s.csv" %fName
+def openFileLocation(self):
+    options = QFileDialog.Options()
+    fileName = QFileDialog.getOpenFileName(self, "QFileDialog.getOpenFileName()", "", "All Files (*)", options=options)
+    return str(fileName)
