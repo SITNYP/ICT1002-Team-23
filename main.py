397ba18aafdaf40060c12a504d6921fcecd0aab9
jellyvongelatin <how2pocketsg@gmail.com>
2019-10-11 18:53:54 +0800

    Add files via upload
    Import, Export
    Displaying CSV
    Sorting Table
    Searching Table
    
    is up
    
    Left displaying plot with matplotlib (for Dylan)

--- main.py
+++ main.py
@@ -3,11 +3,17 @@ import pandas as pd
 import modules as mod
 import sys
 import collections
+from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.backends.backend_qt4agg import NavigationToolbar2QT as NavigationToolbar
+from matplotlib.figure import Figure
+import matplotlib.pyplot as plt
+from pandas.api.types import is_numeric_dtype, is_float_dtype, is_string_dtype
 
 Qt = QtCore.Qt
 
 
 class PandasModel(QtCore.QAbstractTableModel):
+    """Formats CSV into Pandas Dataframe Object"""
     def __init__(self, data, parent=None):
         QtCore.QAbstractTableModel.__init__(self, parent)
         self._data = data
@@ -35,18 +41,31 @@ class PandasModel(QtCore.QAbstractTableModel):
 
 
 class MainWindow(QtGui.QMainWindow):
+    """Main GUI"""
     table = pd.DataFrame()
+    view = pd.DataFrame()
+
+
     def __init__(self):
         super(MainWindow, self).__init__()
         uic.loadUi('Main.ui', self)
         self.setWindowTitle("Team 23 Python Data Analyser")
 
+        self.typeBox.addItem("Line Graph")
+        self.plotBox.addItem("None")
+
         self.actionImport_CSV.activated.connect(self.importCSV)  #imports the csv to the program
         self.actionMerge_CSV.activated.connect(self.mergeCSV)    #merges another csv with the current one
+        self.actionExport_CSV.activated.connect(self.exportCSV)  #exports current table (after modifications) as a csv file
+        self.searchView.pressed.connect(self.searchTable)       #Searches table and displays a view
+        self.clearView.pressed.connect(self.clearTableView)     #clears the current search view
+
+        self.generate.clicked.connect(self.displayPlot)         #displays plotted graph according to parameters
 
         self.show()
 
-    def importCSV(self):  # imports csv and displays as table in GUI
+    def importCSV(self):
+        """Imports CSV and displays as table in GUI"""
         try:
             fileName = mod.openFileLocation(self)
             self.table = mod.fileUpload(fileName)
@@ -56,11 +75,19 @@ class MainWindow(QtGui.QMainWindow):
             self.csvTable.resizeColumnsToContents()
             self.csvTable.show()
 
+            #populate combobox values
+            for i in self.table.columns:
+                self.xBox.addItem(i)
+                self.yBox.addItem(i)
+                self.plotBox.addItem(i)
+
         except Exception as e:
-            #display error gui
-            print e
+            #Display error message
+            mod.errorGUI(str(e))
+
 
     def mergeCSV(self):
+        """Merges another CSV with the currently open Pandas Dataframe"""
         try:
             fileName = mod.openFileLocation(self)
             table2 = mod.fileUpload(fileName)
@@ -69,7 +96,8 @@ class MainWindow(QtGui.QMainWindow):
                 newTable = pd.concat([self.table, table2])
                 newTable = newTable.reset_index(drop=True)
             else:
-                newTable = pd.concat([self.table, table2], axis=1)
+                return ValueError("No matching columns found!")
+                #newTable = pd.concat([self.table, table2], axis=1)
 
             self.table = newTable
 
@@ -78,9 +106,105 @@ class MainWindow(QtGui.QMainWindow):
             self.csvTable.resizeColumnsToContents()
             self.csvTable.show()
 
+
         except Exception as e:
-            #display error gui?
-            print e
+            # Display error message
+            mod.errorGUI(str(e))
+
+    def exportCSV(self):
+        """Exports the current Pandas Dataframe (after merges and modifications) as a CSV File"""
+        try:
+            fileName = mod.saveFileLocation(self)
+            self.table.to_csv(fileName, encoding='utf-8', index=False)
+        except Exception as e:
+            # Display error message
+            mod.errorGUI(str(e))
+
+    def searchTable(self):
+        """Searches the table and presents the search results as a view"""
+        try:
+            self.view = self.view.iloc[0:0]
+            #search table and generates view
+            searchQuery = str(self.search.text())
+
+            if searchQuery == "":
+                raise ValueError("Please enter a search query!")
+
+            #creates dictionary to hold the float, int and str values of the query
+            queryDict = {'string': searchQuery}
+            try:
+                queryDict['int'] = int(searchQuery)
+                queryDict['float'] = float(searchQuery)
+            except:
+                queryDict['int'] = 0
+                queryDict['float'] = 0.0
+
+            #initialises the view dataframe
+
+
+            for i in self.table.columns:
+                loopQ = ""
+                #sets the query datatype according to the type in the column
+                if is_numeric_dtype(self.table[i].dtype):
+                    loopQ = queryDict['int']
+                elif is_string_dtype(self.table[i].dtype):
+                    loopQ = queryDict['string']
+                elif is_float_dtype(self.table[i].dtype):
+                    loopQ = queryDict['float']
+
+                #queries the table to see if it has any matches in the column
+                queryBool = self.table[i] == loopQ
+
+                #if there are matches, add the rows to the view otherwise will show no result.
+                if self.table[queryBool].empty is False:
+                    self.view = pd.concat([self.view, self.table.loc[self.table[i] == loopQ]])
+                else:
+                    continue
+
+            #displays view
+            if not self.view.empty:
+                self.csvTable.setSortingEnabled(True)
+                self.csvTable.setModel(PandasModel(self.view))
+                self.csvTable.resizeColumnsToContents()
+                self.csvTable.show()
+            else:
+                raise ValueError("No Value Found!")
+
+        except Exception as e:
+            mod.errorGUI(str(e))
+
+    def clearTableView(self):
+        """Resets the View to the original table"""
+        self.csvTable.setSortingEnabled(True)
+        self.csvTable.setModel(PandasModel(self.table))
+        self.csvTable.resizeColumnsToContents()
+        self.csvTable.show()
+
+    def displayPlot(self):
+        """Displays Visualisation of Data using MatPlotLib"""
+        try:
+            #retrieve combobox values
+            graphtype = str(self.typeBox.currentText())
+            xaxis = str(self.xBox.currentText())
+            yaxis = str(self.yBox.currentText())
+            plotType = str(self.plotBox.currentText())
+
+            #generates the plot
+
+
+
+            #puts into GUI
+            """
+                canvas = FigureCanvas(fig)
+                canvas.draw()
+                self.Visualisation.addWidget(canvas)
+    
+                toolbar = NavigationToolbar(self.canvas,self.graphView, coordinates=True)
+                self.Visualisation.addWidget(toolbar)
+            """
+        except Exception as e:
+            mod.errorGUI(str(e))
+
 
 if __name__ == '__main__':
     app = QtGui.QApplication(sys.argv)
--- modules.py
+++ modules.py
@@ -1,14 +1,63 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-import numpy as np
-from PyQt4.QtGui import QFileDialog, QTableWidget, QTableWidgetItem, QSortFilterProxyModel
+#import numpy as np
+from PyQt4.QtGui import QFileDialog, QTableWidget, QTableWidgetItem, QSortFilterProxyModel, QMessageBox
 
 def fileUpload(filePath): #takes in the file path and returns it as a pandas csv variable
-    #if filePath.lower().endswith('.csv'):
-    data = pd.read_csv(filePath)
-    return data
+    """Uploads the user's file and returns a Pandas CSV variable"""
+    if filePath.lower().endswith('.csv'):
+        data = pd.read_csv(filePath)
+        return data
+    else:
+        raise ValueError("Please upload a CSV File!")
 
 def openFileLocation(self):
+    """Prompts user to upload a file"""
     options = QFileDialog.Options()
-    fileName = QFileDialog.getOpenFileName(self, "QFileDialog.getOpenFileName()", "", "All Files (*)", options=options)
+    fileName = QFileDialog.getOpenFileName(self, "Open File", "", "All Files (*)", options=options)
     return str(fileName)
+
+def saveFileLocation(self):
+    """Prompts user where to save the file"""
+    options = QFileDialog.Options()
+    fileName = QFileDialog.getSaveFileName(self, "Save as", "", "CSV(*.csv)", options=options)
+    return str(fileName)
+
+def errorGUI(error):
+    # Display error messsage
+    warning = QMessageBox()
+    warning.setIcon(QMessageBox.Critical)
+    warning.setWindowTitle("Error!")
+    warning.setText(error)
+    warning.exec_()
+
+# takes in 4 variables, dataset (integrate with file), x axis to plot (int - col number),
+# y axis to plot (int - col number), the groups to plot by (set to none by default, if no groupby is selected, the arguement is null)
+
+
+def plot(data, graphType, x, y, desiredPlots=None):
+    if desiredPlots != "None":
+        #groups the specified column
+        fig, ax = plt.subplots()
+        groupedData = data.groupby(data.columns[desiredPlots])
+        for key, item in groupedData:
+            groups = groupedData.get_group(key)
+            #plots the graph
+            groups.plot(kind=graphType, x=data.columns[x], y=data.columns[y], ax=ax, label=key, figsize=(16, 6))
+            #sets labels
+            plt.xlabel(data.columns[x])
+            plt.ylabel(data.columns[y])
+            #plt.title("add file name here")
+        #plt.show()
+        return ax
+    else:
+        #plots the graph
+        graph = data.plot(kind=graphType, x=data.columns[x], y=data.columns[y], figsize=(16, 6))
+        #sets labels
+        plt.xlabel(data.columns[x])
+        plt.ylabel(data.columns[y])
+        #plt.title("add file name here")
+        #plt.show()
+        return graph
+
+#main problem is that when there are multiple groups, it will not take the dataset well. E.g. multiple groupby we can do
