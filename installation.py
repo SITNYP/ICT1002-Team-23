284ae6d8175aaf1f4c99fc0178878118b578cb24
jellyvongelatin <how2pocketsg@gmail.com>
2019-10-22 17:19:03 +0800

    Update installation.py

--- installation.py
+++ installation.py
@@ -1,10 +1,14 @@
 from pip._internal import main as pipmain
 
-pipmain(['install', 'pandas'])
-pipmain(['install', '.\pyqt4 package\PyQt4-4.11.4-cp27-cp27m-win_amd64.whl'])
-pipmain(['install', 'matplotlib'])
-pipmain(['install', 'pdfkit'])
-pipmain(['install', 'imgkit'])
-pipmain(['install', 'sklearn'])
+try:
+  pipmain(['install', '.\packages\pandas-0.24.2-cp27-cp27m-win_amd64.whl'])
+  pipmain(['install', '.\packages\PyQt4-4.11.4-cp27-cp27m-win_amd64.whl'])
+  pipmain(['install', 'matplotlib'])
+  pipmain(['install', 'pdfkit'])
+  pipmain(['install', 'imgkit'])
+  pipmain(['install', 'scipy'])
+  pipmain(['install', 'scikit-learn'])
 
-print "Dependecies installed! You are now ready to run the Data Analyser."
+  print "Dependecies installed! You are now ready to run the Data Analyser."
+except:
+  print "An error occurred! Please ensure that Pip is up-to-date and try again!"
