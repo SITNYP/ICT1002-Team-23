0492e4cbb958b01ae91dc9ffb1e095429a50e13b
jellyvongelatin <how2pocketsg@gmail.com>
2019-10-23 10:34:12 +0800

    Add files via upload

--- installation.py
+++ installation.py
@@ -1,14 +1,19 @@
-from pip._internal import main as pipmain
+import subprocess
+import sys
+
+def installWheel(package):
+    subprocess.call([sys.executable, "-m", "pip", "install", package])
+    
+def install(package):
+    subprocess.call([sys.executable, "-m", "pip", "install", "-r", package])
 
 try:
-  pipmain(['install', '.\packages\pandas-0.24.2-cp27-cp27m-win_amd64.whl'])
-  pipmain(['install', '.\packages\PyQt4-4.11.4-cp27-cp27m-win_amd64.whl'])
-  pipmain(['install', 'matplotlib'])
-  pipmain(['install', 'pdfkit'])
-  pipmain(['install', 'imgkit'])
-  pipmain(['install', 'scipy'])
-  pipmain(['install', 'scikit-learn'])
+    
+    installWheel('.\packages\pandas-0.24.2-cp27-cp27m-win_amd64.whl')
+    installWheel('.\packages\PyQt4-4.11.4-cp27-cp27m-win_amd64.whl')
+    install('requirements.txt')
+    
 
-  print "Dependecies installed! You are now ready to run the Data Analyser."
+    print "Dependecies installed! You are now ready to run the Data Analyser."
 except:
-  print "An error occurred! Please ensure that Pip is up-to-date and try again!"
+    print "An error occurred! Please ensure that Pip is up-to-date and try again!"
